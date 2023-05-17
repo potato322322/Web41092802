56a5d35d19c33701e19eb80ce3365b9e667cde19
potatp <songgaqp44@gmail.com>
2023-05-17 19:52:59 +0800

    1

---
+++ Web410928092/Controllers/WaterController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410928092.Controllers
+{
+    public class WaterController : Controller
+    {
+        // GET: Water
+        public ActionResult Index(String name,float? weight)
+        {
+           
+            var result = "";
+            if (weight.HasValue)
+            {
+                result = (weight * 30) + "cc";
+            }
+            ViewBag.Name = name;
+            ViewBag.Result = result;
+            return View();
+        }
+    }
+}
