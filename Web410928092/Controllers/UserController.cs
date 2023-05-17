77a26e6b9ce4626e3f4ebdbb0da3e663165c6c65
potatp <songgaqp44@gmail.com>
2023-05-17 20:49:25 +0800

    11

---
+++ Web410928092/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410928092.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        public ActionResult SignUp(string name, string account, string password)
+        {
+            if (name.IsNullOrWhiteSpace())
+            { 
+                ViewBag.NameMessage = "請輸入姓名";
+            }
+            {
+                ViewBag.NameMessage = "請輸入姓名";
+            }
+            {
+                ViewBag.NameMessage = "請輸入姓名";
+            }
+            
+            return View();
+        }
+    }
+}
